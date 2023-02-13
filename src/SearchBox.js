6d53079f6b2e4deeb0ebb27e561d36afa6020925
Modriel <modriel@gmail.com>
2023-02-13 18:40:09 +0100

    asd

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div className="pa2">
+
+            <input
+                type='search'
+                className='pa3 ba b--green bg-light-'
+                placeholder='search robots' />
+        </div>
+    );
+}
+
+export default SearchBox
